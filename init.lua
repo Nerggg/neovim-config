5296bd3f35fef2860b50d1f7a96c350f1d56c754
nerggg <13522147@std.stei.itb.ac.id>
2025-06-09 16:14:28 +0700

    feat: add A-a to move to the end of line

--- init.lua
+++ init.lua
@@ -369,6 +369,7 @@ vim.api.nvim_set_keymap('i', '<A-h>', '<Left>', { noremap = true, silent = true 
 vim.api.nvim_set_keymap('i', '<A-j>', '<Down>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-k>', '<Up>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-l>', '<Right>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<A-a>', '<End>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('i', '<A-s>', '<Esc>viw', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<A-s>', 'viw', { noremap = true, silent = true })
 
