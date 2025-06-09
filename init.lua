8c484a3b2e637cd1616f68158ace2ef144bb6e71
nerggg <13522147@std.stei.itb.ac.id>
2025-06-09 15:13:41 +0700

    feat: remove snippet

--- init.lua
+++ init.lua
@@ -46,8 +46,6 @@ vim.cmd('Plug \'hrsh7th/cmp-nvim-lsp\'') -- LSP source for nvim-cmp
 vim.cmd('Plug \'hrsh7th/cmp-buffer\'') -- Buffer source for nvim-cmp
 vim.cmd('Plug \'hrsh7th/cmp-path\'') -- Path source for nvim-cmp
 vim.cmd('Plug \'L3MON4D3/LuaSnip\'') -- Snippet engine
-vim.cmd('Plug \'saadparwaiz1/cmp_luasnip\'') -- Luasnip source for nvim-cmp
-vim.cmd('Plug \'rafamadriz/friendly-snippets\'') -- Predefined snippets
 -- LSP STUFFS END
 
 -- Telescope plugins
@@ -213,18 +211,9 @@ local lspconfig = require('lspconfig')
 
 -- Configure nvim-cmp for autocompletion
 local cmp = require('cmp')
-local luasnip = require('luasnip')
-
--- Load VSCode-style snippets
-require('luasnip.loaders.from_vscode').lazy_load()
 
 -- Setup nvim-cmp with mappings and sources
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -234,8 +223,6 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -243,8 +230,6 @@ cmp.setup({
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
@@ -252,8 +237,6 @@ cmp.setup({
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  }, {
     { name = 'buffer' },
     { name = 'path' },
   })
